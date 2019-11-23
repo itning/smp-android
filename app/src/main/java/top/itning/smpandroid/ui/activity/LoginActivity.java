d4696a1079da91ffe90c6b2fd66463bc4e236c31
itning <itning@itning.top>
2019-11-23 15:57:34 +0800

    login activity

--- app/src/main/java/top/itning/smpandroid/ui/activity/LoginActivity.java
+++ app/src/main/java/top/itning/smpandroid/ui/activity/LoginActivity.java
@@ -1,16 +1,25 @@
 package top.itning.smpandroid.ui.activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,6 +41,10 @@ public class LoginActivity extends AppCompatActivity {
     CustomVideoView videoView;
     @BindView(R2.id.iv_background)
     AppCompatImageView imageView;
+    @BindView(R2.id.tl_username)
+    TextInputLayout usernameLayout;
+    @BindView(R2.id.tl_password)
+    TextInputLayout passwordLayout;
     private Disposable disposable;
 
     @Override
@@ -45,6 +58,59 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        passwordLayout.setEndIconMode(TextInputLayout.END_ICON_PASSWORD_TOGGLE);
+        EditText usernameLayoutEditText = usernameLayout.getEditText();
+        EditText passwordLayoutEditText = passwordLayout.getEditText();
+        if (usernameLayoutEditText != null) {
+            usernameLayoutEditText.setSingleLine();
+            usernameLayoutEditText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    usernameLayout.setError(null);
+                }
+            });
+        }
+        if (passwordLayoutEditText != null) {
+            passwordLayoutEditText.setSingleLine();
+            passwordLayoutEditText.setOnEditorActionListener((v, actionId, event) -> {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (inputMethodManager != null) {
+                        inputMethodManager.hideSoftInputFromWindow(getWindow().getDecorView().getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
+                    }
+                    handleLoginBtn(v);
+                    return true;
+                }
+                return false;
+            });
+            passwordLayoutEditText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    passwordLayout.setError(null);
+                }
+            });
+        }
+
         //设置播放加载路径
         // Uri.parse("file:///android_asset/aa.mp4")
         videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.login));
@@ -73,21 +139,50 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void handleLoginBtn(View view) {
+        String username = "", password = "";
+        EditText usernameLayoutEditText = usernameLayout.getEditText();
+        EditText passwordLayoutEditText = passwordLayout.getEditText();
+        if (usernameLayoutEditText != null) {
+            if (usernameLayoutEditText.getText().length() == 0) {
+                usernameLayout.setError("请输入用户名");
+                return;
+            }
+            username = usernameLayoutEditText.getText().toString();
+        }
+        if (passwordLayoutEditText != null) {
+            if (passwordLayoutEditText.getText().length() == 0) {
+                passwordLayout.setError("请输入密码");
+                return;
+            }
+            password = passwordLayoutEditText.getText().toString();
+        }
+        if ("".equals(username) || "".equals(password)) {
+            return;
+        }
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("正在登录");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
         disposable = HttpHelper.get(LoginClient.class)
-                .login("a", "a")
+                .login(username, password)
                 .subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(restModel -> {
+                    progressDialog.dismiss();
                     String token = restModel.getData();
                     if (token != null && !"".equals(token.trim())) {
                         SharedPreferences sharedPreferences = getSharedPreferences(App.SHARED_PREFERENCES_OWN, MODE_PRIVATE);
                         if (sharedPreferences.edit().putString(HttpHelper.TOKEN_KEY, token).commit()) {
                             startActivity(new Intent(this, MainActivity.class));
                             finish();
+                            return;
                         }
                     }
+                    Toast.makeText(this, "登陆失败", Toast.LENGTH_LONG).show();
                 }, throwable -> {
                     Log.w(TAG, "登陆失败", throwable);
+                    progressDialog.dismiss();
                     Toast.makeText(this, throwable.getMessage(), Toast.LENGTH_LONG).show();
                 });
     }
